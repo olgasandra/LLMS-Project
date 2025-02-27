2cecbb717c11c5d5e1e0fd7d2b6ebc6031aaed0c
Joshua Onalaja <joshua.onalaja@ons.gov.uk>
2025-02-27 15:24:53 +0000

    update READme with how to create venv, updated requirements.txt, started to modularise rag_system notebook, update gitignore

--- README.md
+++ README.md
@@ -9,6 +9,18 @@
 ## 1) Introduction
 
 ## 2) Installation
+      
+      
+      You may want to use a virtual environment.
+
+      ...
+      python -m venv NISR_LLM
+      python3.11 -m venv NISR_LLM (if wanting specific python version)
+      NISR_LLM\Scripts\activate
+
+      python -m pip install --upgrade pip
+      pip install -r requirements.txt
+      ...
 
 ## 3) Usage
 
--- src/embedding/embedding.py
+++ src/embedding/embedding.py
@@ -3,3 +3,44 @@
 # move the functions you have made here
 
 # you will need to make the functions global
+
+# add docstrings
+
+# import packages
+import openai
+import pandas as pd
+import numpy as np
+
+client = openai
+
+# Function to generate embeddings
+def get_embedding(text, model):
+    """_summary_
+
+    Args:
+        text (_type_): _description_
+        model (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """    
+    # Replace newlines with spaces to avoid errors
+    text = text.replace("\n", " ")
+    # Calling the OpenAI API to create embeddings
+    response = client.Embedding.create(input=text, model=model)
+    # Return the embedding vector
+    return response['data'][0]['embedding']
+
+# Function to calculate similarity (dot product)
+def calculate_similarity(query_embedding, db_embeddings):
+    """_summary_
+
+    Args:
+        query_embedding (_type_): _description_
+        db_embeddings (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """    
+    similarities = np.dot(db_embeddings, query_embedding)
+    return similarities
