c7de437bb36ff44ba664f7df29ff9585167a7c1a
Hangudieume <hangudieume@gmail.com>
2025-03-20 20:50:28 +0200

    Modified the source code with new RAG functions and added new embeddings data

--- src/embedding_functions.py
+++ src/embedding_functions.py
@@ -1,61 +1,13 @@
-# script with functions for embedding
-
-# move the functions you have made here
-
-# you will need to make the functions global
-
-# add docstrings
-
-# import packages
 import openai
-import pandas as pd
 import numpy as np
-
-client = openai
-
 
 # Function to generate embeddings
 def get_embedding(text, model="text-embedding-ada-002"):
     text = text.replace("\n", " ")
-    response = client.Embedding.create(input=text, model=model)
+    response = openai.Embedding.create(input=text, model=model)
     return response['data'][0]['embedding']
 
 # Function to calculate similarity (dot product)
 def calculate_similarity(query_embedding, db_embeddings):
     similarities = np.dot(db_embeddings, query_embedding)
     return similarities
-
-
-
-
-# # Function to generate embeddings
-# def get_embedding(text, model="text-embedding-ada-002"):
-#     """_summary_
-
-#     Args:
-#         text (_type_): _description_
-#         model (_type_): _description_
-
-#     Returns:
-#         _type_: _description_
-#     """    
-#     # Replace newlines with spaces to avoid errors
-#     text = text.replace("\n", " ")
-#     # Calling the OpenAI API to create embeddings
-#     response = client.Embedding.create(input=text, model=model)
-#     # Return the embedding vector
-#     return response['data'][0]['embedding']
-
-# # Function to calculate similarity (dot product)
-# def calculate_similarity(query_embedding, db_embeddings):
-#     """_summary_
-
-#     Args:
-#         query_embedding (_type_): _description_
-#         db_embeddings (_type_): _description_
-
-#     Returns:
-#         _type_: _description_
-#     """    
-#     similarities = np.dot(db_embeddings, query_embedding)
-#     return similarities
--- src/rag_system.py
+++ src/rag_system.py
@@ -5,82 +5,19 @@ import numpy as np
 from pathlib import Path
 from dotenv import load_dotenv
 import os
-from generation_functions import query_system, generate_response
+from generation_functions import RAG_query_system
 
-# %%
-# Set up directories
 EMBEDDINGS_DIR = Path.cwd().joinpath("data/embeddings")
 
-# %%
-client = openai
 
-# %%
-# Load environment variables from the .env file
-load_dotenv()
-
-# Access environment variables as if they came from the actual environment
-api_key = os.getenv("OPENAI_API_KEY")
-
-# %% [markdown]
-# ### Testing the RAG System
-
-# %%
 # Importing embeddings data
 embeddings_data = pd.read_pickle(EMBEDDINGS_DIR / "isco_08_df_with_embeddings_for_translation.pkl")
 
 
 # write a query
-description = "logging climber"
+description = "akora akazi ko kubaka amazu"
 
-# Retrieve the most relevant row
-response = query_system(description, isco_08_df_pkl)
+# generating a response of the query
+response = RAG_query_system(description, embeddings_data)
 
 print(response)
-
-
-
-# # %% [markdown]
-# # **RAG SYSTEM**
-
-# # %%
-# # Importing the necessary libraries
-# import openai
-# import pandas as pd
-# import numpy as np
-# from pathlib import Path
-# from dotenv import load_dotenv
-# import os
-# from generation_functions import query_system, generate_response
-
-# # %%
-# # Set up directories
-# EMBEDDINGS_DIR = Path.cwd().joinpath("data/embeddings")
-
-# # %%
-# client = openai
-
-# # %%
-# # Load environment variables from the .env file
-# load_dotenv()
-
-# # Access environment variables as if they came from the actual environment
-# api_key = os.getenv("OPENAI_API_KEY")
-
-# # %% [markdown]
-# # ### Testing the RAG System
-
-# # %%
-# # Importing embeddings data
-# embeddings_data = pd.read_pickle(EMBEDDINGS_DIR / "isco_08_df_with_embeddings.pkl")
-
-# # %%
-# query = "This person works as a waiter at restaurant?"
-
-# # Retrieve the most relevant row
-# retrieved_row = query_system(query, embeddings_data)
-
-# # Generate the response
-# response = generate_response(query, retrieved_row)
-# print(response)
-
-
